36178504e484a49ff809d49f9a139a943354b388
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2024-03-23 16:24:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const showcase = document.querySelector('.showcase');
 menuToggle.addEventListener('click', () => { 
   menuToggle.classList.toggle('active'); 
   showcase.classList.toggle('active');
+  
 })
